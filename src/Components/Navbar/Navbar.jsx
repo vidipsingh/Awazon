c505234f7b1bf4b2c5de6b0e8f4523f26322d015
Vidip <vidip2025@gmail.com>
2024-06-08 22:32:14 +0530

    Initial Files Upload

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import logo from "../Assets/logo.png"
+import cart_icon from "../Assets/cart_icon.png"
+import { Link } from 'react-router-dom'
+
+
+const Navbar = () => {
+
+    const [menu,setMenu] = useState("shop");
+
+  return (
+    <div className="navbar">
+        <div className="nav-logo">
+            <img src={logo} alt="" />
+            <p>Shopper</p>
+        </div>
+
+        {/* When a menu item is clicked, the setMenu function updates the menu state to reflect the selected item, and a horizontal rule (<hr/>) is conditionally rendered under the selected item to visually indicate the selection. */}
+        <ul className="nav-menu">
+            <li onClick={() => {setMenu("shop")}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link>{menu==="shop" ? <hr/>:<></>}</li>
+            <li onClick={() => {setMenu("mens")}}><Link style={{textDecoration:'none'}} to='mens'>Men</Link>{menu==="mens" ? <hr/>:<></>}</li>
+            <li onClick={() => {setMenu("womens")}}><Link style={{textDecoration:'none'}} to='womens'>Women</Link>{menu==="womens" ? <hr/>:<></>}</li>
+            <li onClick={() => {setMenu("kids")}}><Link style={{textDecoration:'none'}} to='kids'>Kids</Link>{menu==="kids" ? <hr/>:<></>}</li>
+        </ul>
+
+        <div className="nav-login-cart">
+            <Link to='login'><button>Login</button></Link>
+           <Link to='cart'><img src={cart_icon} alt="" /></Link> 
+            <nav className="nav-cart-count">0</nav>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/Pages/LoginSignup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const LoginSignUp = () => {
+  return (
+    <div>
+      
+    </div>
+  ) 
+}
+
+
+
+
+export default LoginSignUp
+
+
